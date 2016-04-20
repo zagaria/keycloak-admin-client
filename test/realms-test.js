46c702da28230592addf64263dd97939bb882bcb
Lucas Holmquist <lholmqui@redhat.com>
2016-04-20 14:47:52 -0400

    add tests for importRealm and deleteRealm

--- test/realms-test.js
+++ test/realms-test.js
@@ -21,8 +21,7 @@ test('Test getting the list of Realms', (t) => {
       // The listOfRealms should be an Array
       t.equal(listOfRealms instanceof Array, true, 'the list of realms should be an array');
 
-      // There should only be 1 realm and it should be master.
-      t.equal(listOfRealms.length, 1, 'There should only be 1 realm in our test server');
+      //There should be a master realm and it should be the first in the list.
       t.equal(listOfRealms[0].realm, 'master', 'The realm should be named master');
       t.end();
     });
@@ -69,3 +68,83 @@ test('Top-Level Realm Test -  no realm name should through an error', (t) => {
   });
 });
 
+test('Test import a realm - just using a realm name', (t) => {
+  const kca = keycloakAdminClient(settings);
+
+  // A minimal JSON representation of a realm,  just using the name property
+  const realmToAdd = {
+    realm: 'testRealm'
+  };
+
+  kca.then((client) => {
+    t.equal(typeof client.importRealm, 'function', 'The client object returned should have a importRealm function');
+
+    client.importRealm(realmToAdd).then((addedRealm) => {
+      // The importRealm Endpoint does not return anything in the body.
+      // But our api "fakes it" by calling the client.realm(realm) function after a succesfull import.
+      t.equal(addedRealm.realm, realmToAdd.realm, 'The realm should be named ' + realmToAdd.realm);
+
+      // clean up the realm we just added. This is only really needed when running tests locally.
+      // deleteRealm is tested later on
+      // TODO: find a better way
+      client.deleteRealm(realmToAdd.realm);
+      t.end();
+    });
+  });
+});
+
+test('Test import a realm - a not unique name', (t) => {
+  const kca = keycloakAdminClient(settings);
+
+  // A minimal JSON representation of a realm,  just using the name property
+  const realmToAdd = {
+    realm: 'master'
+  };
+
+  kca.then((client) => {
+    client.importRealm(realmToAdd).catch((err) => {
+      t.equal(err.errorMessage, 'Realm with same name exists', 'Error message should be returned when using a non-unique realm name');
+      t.end();
+    });
+  });
+});
+
+test('Test delete a realm', (t) => {
+  const kca = keycloakAdminClient(settings);
+
+  // First we need to create a realm to delete
+  // A minimal JSON representation of a realm,  just using the name property
+  const realmToAdd = {
+    realm: 'testRealmForDeleting'
+  };
+
+  kca.then((client) => {
+    client.importRealm(realmToAdd).then((addedRealm) => {
+      // just a quick quick that the realm is there
+      t.equal(addedRealm.realm, realmToAdd.realm, 'The realm should be named ' + realmToAdd.realm);
+
+      // Call the deleteRealm api to remove this realm
+      client.deleteRealm(realmToAdd.realm).then(() => {
+        // There is no return value on a delete
+        t.end();
+      });
+    });
+  });
+});
+
+test('Test delete a realm that doesn\'t exist', (t) => {
+  const kca = keycloakAdminClient(settings);
+
+  // A minimal JSON representation of a realm,  just using the name property, This realm shouldn't exist
+  const realmToAdd = {
+    realm: 'testRealmThatDoesntExist'
+  };
+
+  kca.then((client) => {
+    // Call the deleteRealm api to remove this realm
+    client.deleteRealm(realmToAdd.realm).catch((err) => {
+      t.equal(err, 'Realm not found.', 'Realm not found should be returned if the realm wasn\'t found to delete');
+      t.end();
+    });
+  });
+});
