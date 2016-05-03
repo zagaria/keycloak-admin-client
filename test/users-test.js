8153334bfff2c8258c5f5878459dbb538158175f
Lucas Holmquist <lholmqui@redhat.com>
2016-05-03 15:26:55 -0400

    migrate the user functions to the new namespacing

--- test/users-test.js
+++ test/users-test.js
@@ -16,12 +16,12 @@ test('Test getting the list of users for a Realm', (t) => {
   const kca = keycloakAdminClient(settings);
 
   kca.then((client) => {
-    t.equal(typeof client.users, 'function', 'The client object returned should have a users function');
+    t.equal(typeof client.users.find, 'function', 'The client object returned should have a users function');
 
     // Use the master realm
     const realmName = 'master';
 
-    client.users(realmName).then((listOfUsers) => {
+    client.users.find(realmName).then((listOfUsers) => {
       // The listOfUsers should be an Array
       t.equal(listOfUsers instanceof Array, true, 'the list of users should be an array');
 
@@ -40,7 +40,7 @@ test('Test getting the list of users for a Realm that doesn\'t exist', (t) => {
     // Use the master realm
     const realmName = 'notarealrealm';
 
-    client.users(realmName).catch((err) => {
+    client.users.find(realmName).catch((err) => {
       t.equal(err, 'Realm not found.', 'Realm not found should be returned if the realm wasn\'t found');
       t.end();
     });
@@ -55,7 +55,7 @@ test('Test getting the one user for a Realm', (t) => {
     const realmName = 'master';
     const userId = 'f9ea108b-a748-435f-9058-dab46ce59771'; // This is the admin user id from /build/kc-setup-for-tests.json
 
-    client.user(realmName, userId).then((user) => {
+    client.users.find(realmName, {userId: userId}).then((user) => {
       t.equal(user.id, userId, 'The userId we used and the one returned should be the same');
       t.equal(user.username, 'admin', 'The username returned should be admin');
       t.end();
@@ -71,7 +71,7 @@ test('Test getting the one user for a Realm - userId doesn\'t exist', (t) => {
     const realmName = 'master';
     const userId = 'not-an-id';
 
-    client.user(realmName, userId).catch((err) => {
+    client.users.find(realmName, {userId: userId}).catch((err) => {
       t.equal(err, 'User not found', 'A User not found error should be thrown');
       t.end();
     });
@@ -82,7 +82,7 @@ test('Test update a users info', (t) => {
   const kca = keycloakAdminClient(settings);
 
   kca.then((client) => {
-    t.equal(typeof client.updateUser, 'function', 'The client object returned should have a updateUser function');
+    t.equal(typeof client.users.update, 'function', 'The client object returned should have a update function');
     // Use the master realm
     const realmName = 'master';
     let testUser = Object.assign({}, kcSetupForTests[0].users.find((user) => { return user.id === '3ff724a6-90a8-4050-9981-4a6def74870a';})); // This is the test1 user id from /build/kc-setup-for-tests.json
@@ -95,9 +95,9 @@ test('Test update a users info', (t) => {
     testUser.firstName = 'Test User 1 is my first name';
     testUser.lastName = 'This is my last name';
 
-    client.updateUser(realmName, testUser).then(() => {
+    client.users.update(realmName, testUser).then(() => {
       // The update doesn't return anything so we need to go get what we just updated
-      return client.user(realmName, testUser.id);
+      return client.users.find(realmName, {userId: testUser.id});
     }).then((user) => {
       t.equal(user.firstName, testUser.firstName, 'The firstName returned should be Test User 1 is my first name');
       t.equal(user.lastName, testUser.lastName, 'The lastName returned should be This is my last name');
@@ -121,7 +121,7 @@ test('Test update a users info - same username error', (t) => {
     // Change the user id to the admin user id, this will create an error since the username/email already exists
     testUser.id = 'f9ea108b-a748-435f-9058-dab46ce59771';
 
-    client.updateUser(realmName, testUser).catch((err) => {
+    client.users.update(realmName, testUser).catch((err) => {
       t.equal(err.errorMessage, 'User exists with same username or email', 'Should return an error message');
       t.end();
     });
@@ -142,7 +142,7 @@ test('Test update a users info - update a user that does not exist', (t) => {
     // Change the user id to something that doesn't exist
     testUser.id = 'f9ea108b-a748-435f-9058-dab46ce5977-not-real';
 
-    client.updateUser(realmName, testUser).catch((err) => {
+    client.users.update(realmName, testUser).catch((err) => {
       console.log(err);
       t.equal(err, 'User not found', 'Should return an error that no user is found');
       t.end();
@@ -154,13 +154,13 @@ test('Test delete a user', (t) => {
   const kca = keycloakAdminClient(settings);
 
   kca.then((client) => {
-    t.equal(typeof client.deleteUser, 'function', 'The client object returned should have a deleteUser function');
+    t.equal(typeof client.users.remove, 'function', 'The client object returned should have a deleteUser function');
 
     // Use the master realm
     const realmName = 'Test Realm 1';
     const userId = '677e99fd-b854-479f-afa6-74f295052770';
 
-    client.deleteUser(realmName, userId).then(() => {
+    client.users.remove(realmName, userId).then(() => {
       t.end();
     });
   });
@@ -173,7 +173,7 @@ test('Test delete a user that doesn\'t exist', (t) => {
   const realmName = 'master';
   kca.then((client) => {
     // Call the deleteRealm api to remove this realm
-    client.deleteUser(realmName, userId).catch((err) => {
+    client.users.remove(realmName, userId).catch((err) => {
       t.equal(err, 'User not found', 'Should return an error that no user is found');
       t.end();
     });
